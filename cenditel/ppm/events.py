fb046634eafae1fc9def9a475921ed4a0d443006
Oswaldo Lopez <oswaldolo@hotmail.com>
2011-06-30 15:51:07 +0000

    agredado evento para envio de correos al editar correos

--- cenditel/ppm/events.py
+++ cenditel/ppm/events.py
@@ -6,11 +6,17 @@ from zope.app.component.hooks import getSite
 
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletAssignmentMapping
+from Products.CMFCore.utils import getToolByName
+
 from cenditel.ppm.portlets import navigation
 from cenditel.ppm.portlets import select_project
 
 from plone.portlets.constants import CONTEXT_ASSIGNMENT_KEY, CONTEXT_CATEGORY
 from plone.app.portlets.storage import PortletAssignmentMapping
+from smtplib import SMTPRecipientsRefused
+from Products.MailHost.MailHost import MailHostError
+from cenditel.ppm import ppmMessageFactory as _
+import logging
 
 DEFAULT_RIGHT_PORTLETS = (
     ('navigation', navigation.Assignment, {}),
@@ -23,6 +29,7 @@ def SavedElement(object, evt, ):
     TotalContributors= []
     object.setContributors(managers)
     Makeportlets(object, evt)
+    SendEmail(object, evt)
     
 def Makeportlets(object, evt):
     annotated = IAnnotations(object)
@@ -41,9 +48,25 @@ def Makeportlets(object, evt):
     portlets['plone.rightcolumn'] = right_portlets
     annotated[CONTEXT_ASSIGNMENT_KEY] = portlets
 
-            
-    #site = getSite()
-    #column = getUtility(IPortletManager, name=u'plone.leftcolumn', context=site)
-    #manager = getMultiAdapter((site, column,), IPortletAssignmentMapping)
-    #import pdb; pdb.set_trace()
-    
+def SendEmail(object, evt):
+    host = getToolByName(object, 'MailHost')
+    message=_(u"""The project entitle %s to which you are subscribed has been modified
+    you can see it in %s """) % (object.title, object.absolute_url())
+    ToList=evt.object.suscribers
+    From=host.smtp_userid
+    Subject=_(u"Modified Content %s") % (object.title)
+    if ToList == "" or ToList==[]:
+        return
+    else:
+        #import pdb; pdb.set_trace()
+        try:
+            import pdb; pdb.set_trace()
+            host.secureSend(message, ToList, From,subject=Subject, subtype='plain', charset='UTF-8')
+         
+        except MailHostError:
+            logger = logging.getLogger("cenditel.ppm.events.SedEmail")
+            logger.warning ("We sorry, we cold not send the email because the mail host was not configure")
+        except SMTPRecipientsRefused:
+            logger = logging.getLogger("cenditel.ppm.events.SedEmail")
+            logger.warning ("We sorry, we cold not send the email because one or more recipiendts has been refused")
+            raise SMTPRecipientsRefused('Recipient address rejected by server')
