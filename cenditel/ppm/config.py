2e9ea281885a144da2e877dcbb525934b1b600aa
Oswaldo Lopez <oswaldolo@hotmail.com>
2011-07-04 02:01:58 +0000

    Se capitalizaron los nombres de las clases de Proyecto en especifico el Add Project de la tupla permisos.

--- cenditel/ppm/config.py
+++ cenditel/ppm/config.py
@@ -5,12 +5,13 @@ from Products.Archetypes.utils import DisplayList
 
 from cenditel.ppm import ppmMessageFactory as _
 
+
 PROJECTNAME = 'cenditel.ppm'
 
 ADD_PERMISSIONS = {
     'folderproj': 'cenditel.ppm: Add folderproj',
     'proposals': 'cenditel.ppm: Add proposals',
-    'project': 'cenditel.ppm: Add project',
+    'Project': 'cenditel.ppm: Add Project',
 }
 
 TYPE_SUBFOLDER_PROJECT = DisplayList((
